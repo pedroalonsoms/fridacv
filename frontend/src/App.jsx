b8ec98cb23bbb40b3963cc8a85b9900fd8104a9b
pedroalonsoms <pedro123ben10@gmail.com>
2023-09-23 18:42:18 -0600

    init register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import RegisterCompanyPage from "./pages/RegisterCompanyPage";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RegisterCompanyPage />;
 }
---
+++ frontend/src/pages/RegisterCompanyPage.jsx
@@ -0,0 +1,11 @@
+export default function RegisterCompanyPage() {
+  return (
+    <>
+      <h1>Register Company</h1>
+      <form action="#">
+        <p>Welcome</p>
+        <label htmlFor="fname">First name:</label>
+      </form>
+    </>
+  );
+}
