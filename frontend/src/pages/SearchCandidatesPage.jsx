d361308023b9bcb286d66fdbbb2f28c23387ac04
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-09-24 01:43:02 -0600

    Added QR Code

--- frontend/src/pages/SearchCandidatesPage.jsx
+++ frontend/src/pages/SearchCandidatesPage.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../components/Navbar";
 import Candidate from "../components/Candidate";
 import Select from "react-select";
+import { Dialog } from "@headlessui/react";
 import { useState } from "react";
+import { QRCodeSVG } from "qrcode.react";
 
 export default function SearchCandidatesPage() {
+  let [isOpen, setIsOpen] = useState(true);
+
   const [keywords, setKeywords] = useState([]);
   const options = [
     { value: "felipe", label: "Felipe" },
@@ -17,10 +21,41 @@ export default function SearchCandidatesPage() {
 
   return (
     <>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 grid place-items-center ">
+          {/* The actual dialog panel  */}
+          <Dialog.Panel className="flex w-full max-w-md flex-col items-center rounded-md bg-white p-6">
+            <Dialog.Title className="text-lg font-semibold">
+              Your QR Code
+            </Dialog.Title>
+            <QRCodeSVG value="https://reactjs.org/" className="m-4" />
+            <button
+              className="rounded-md border border-slate-400 bg-white px-4 py-2 text-black"
+              onClick={() => setIsOpen(false)}
+            >
+              Close
+            </button>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+
       <Navbar />
-      <h1 className="py-20 text-center text-6xl font-bold">
-        Search Candidates
-      </h1>
+
+      <div className="mx-auto flex max-w-2xl items-center justify-between">
+        <h2 className="py-20 text-center text-6xl font-bold">
+          Search Candidates
+        </h2>
+        <button
+          className="rounded-md bg-black px-4 py-2 text-lg text-white"
+          onClick={() => setIsOpen(true)}
+        >
+          View QR
+        </button>
+      </div>
 
       <form onSubmit={handleSearch} className="mx-auto flex max-w-2xl gap-2">
         <Select
