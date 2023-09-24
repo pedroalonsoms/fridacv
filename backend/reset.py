c32dc4a7fe2eaca3fc07dedb89233129db1dbe32
pedroalonsoms <pedro123ben10@gmail.com>
2023-09-24 00:59:17 -0600

    fix flask server

--- backend/reset.py
+++ backend/reset.py
@@ -1,9 +1,19 @@
 import sqlite3
+
 conn = sqlite3.connect('FridaCV.db')
-cursor = conn.cursor() # con el cursor se hacen consultas
+cursor = conn.cursor()
+
+# DROP TABLES
+cursor.execute('DROP TABLE ConfirmedCandidates')
+cursor.execute('DROP TABLE CompanyPosition')
+cursor.execute('DROP TABLE Company')
+cursor.execute('DROP TABLE Redflags')
+cursor.execute('DROP TABLE Softskills')
+cursor.execute('DROP TABLE Hardskills')
+cursor.execute('DROP TABLE URL')
+cursor.execute('DROP TABLE Candidate')
 
 # Tablas del procesamiento de CVs por la IA
-#cursor.execute('CREATE TABLE IF NOT EXISTS Candidate (id_candidate INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), password VARCHAR(50), mail VARCHAR(50), cv_route VARCHAR(50), longevity BOOLEAN)')
 cursor.execute('CREATE TABLE IF NOT EXISTS Candidate (id_candidate INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100), email VARCHAR(50), cv_route VARCHAR(50))')
 cursor.execute('CREATE TABLE IF NOT EXISTS URL (id_candidate INTEGER, url VARCHAR(100), source VARCHAR(50), FOREIGN KEY (id_candidate) REFERENCES Candidate(id_candidate))')
 cursor.execute('CREATE TABLE IF NOT EXISTS Hardskills (id_candidate INTEGER, hardskill VARCHAR(50), word_repetition INTEGER, FOREIGN KEY (id_candidate) REFERENCES Candidate(id_candidate))')
@@ -13,8 +23,7 @@ cursor.execute('CREATE TABLE IF NOT EXISTS Redflags (id_candidate INTEGER, descr
 # Tablas de la informacion del sitio web de job hunting
 cursor.execute('CREATE TABLE IF NOT EXISTS Company (id_company INTEGER PRIMARY KEY AUTOINCREMENT, company_name VARCHAR(50), email VARCHAR(50), password VARCHAR(50))')
 cursor.execute('CREATE TABLE IF NOT EXISTS CompanyPosition (id_company INTEGER, position_name VARCHAR(50), position_description VARCHAR(500), FOREIGN KEY (id_company) REFERENCES Company(id_company))')
-cursor.execute('CREATE TABLE IF NOT EXISTS ConfirmedCandidates (id_company, id_candidate, FOREIGN KEY (id_company) REFERENCES Company(id_company), FOREIGN KEY (id_candidate) REFERENCES Candidate(id_candidate))')
+cursor.execute('CREATE TABLE IF NOT EXISTS ConfirmedCandidates (id_company INTEGER, id_candidate INTEGER, FOREIGN KEY (id_company) REFERENCES Company(id_company), FOREIGN KEY (id_candidate) REFERENCES Candidate(id_candidate))')
 
 conn.commit() # confirmando cambios en la BD
-
 conn.close()
