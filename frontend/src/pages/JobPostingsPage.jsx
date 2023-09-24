f8c622e3ebbee1c7988a4980babedf5256436752
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-09-24 01:15:10 -0600

    Job Postings Fetch

--- frontend/src/pages/JobPostingsPage.jsx
+++ frontend/src/pages/JobPostingsPage.jsx
@@ -6,6 +6,42 @@ import { useState } from "react";
 export default function JobPostingsPage() {
   let [isOpen, setIsOpen] = useState(true);
 
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("handlesubmit");
+
+    // Enviar los datos al servidor
+    try {
+      const response = await fetch("/api/crearRegistro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 201) {
+        console.log("Registro creado con éxito");
+        setIsOpen(false); // Cerrar el diálogo después del envío exitoso
+      } else {
+        console.error("Error al enviar datos");
+      }
+      setIsOpen(false);
+    } catch (error) {
+      console.error("Error al enviar datos:", error);
+    }
+  };
+
   return (
     <>
       <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
@@ -20,44 +56,51 @@ export default function JobPostingsPage() {
               New Job Posting
             </Dialog.Title>
 
-            <div>
-              <label htmlFor="title" className="block pt-3">
-                Title
-              </label>
-              <input
-                type="title"
-                id="title"
-                name="title"
-                className="w-full rounded-md border border-slate-400 p-2"
-              ></input>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="title" className="block pt-3">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                  className="w-full rounded-md border border-slate-400 p-2"
+                ></input>
+              </div>
 
-            <div>
-              <label htmlFor="description" className="block pt-2">
-                Description
-              </label>
-              <input
-                type="description"
-                id="description"
-                name="description"
-                className="w-full rounded-md border border-slate-400 p-2"
-              ></input>
-            </div>
+              <div>
+                <label htmlFor="description" className="block pt-2">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  id="description"
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                  className="w-full rounded-md border border-slate-400 p-2"
+                ></input>
+              </div>
 
-            <div className="mt-3 flex justify-end gap-2">
-              <button
-                className="rounded-md border border-slate-400 bg-white px-4 py-2 text-black"
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-              </button>
-              <button
-                className="rounded-md bg-black px-4 py-2 text-white"
-                onClick={() => setIsOpen(false)}
-              >
-                Continue
-              </button>
-            </div>
+              <div className="mt-3 flex justify-end gap-2">
+                <button
+                  className="rounded-md border border-slate-400 bg-white px-4 py-2 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="rounded-md bg-black px-4 py-2 text-white"
+                  // onClick={() => setIsOpen(false)}
+                >
+                  Continue
+                </button>
+              </div>
+            </form>
           </Dialog.Panel>
         </div>
       </Dialog>
