001a411e1fe712cdcfcc1a42e239c47e826bef6f
pedroalonsoms <pedro123ben10@gmail.com>
2023-09-23 19:11:58 -0600

    advancing register page

--- frontend/src/pages/RegisterCompanyPage.jsx
+++ frontend/src/pages/RegisterCompanyPage.jsx
@@ -1,10 +1,30 @@
 export default function RegisterCompanyPage() {
   return (
     <>
-      <h1>Register Company</h1>
-      <form action="#">
-        <p>Welcome</p>
-        <label htmlFor="fname">First name:</label>
+      <h1 className="pt-20 text-center text-6xl font-bold">Register Company</h1>
+      <div className="pt-20"></div>
+      <form
+        action="#"
+        className="mx-auto max-w-md rounded-md border border-slate-400 p-4"
+      >
+        <p className="text-slate-400">Welcome</p>
+
+        <div>
+          <label htmlFor="companyName">First name</label>
+          <input type="text" id="companyName" name="companyName"></input>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email"></input>
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password"></input>
+        </div>
+
+        <button type="submit">Save changes</button>
       </form>
     </>
   );
