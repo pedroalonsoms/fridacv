3dea8f0b724906ba3d8a7b7556918cceb07063b5
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-09-23 21:53:24 -0600

    Changed to Components

--- frontend/src/pages/SearchCandidatesPage.jsx
+++ frontend/src/pages/SearchCandidatesPage.jsx
@@ -1,3 +1,5 @@
+import Candidate from "../components/Candidate";
+
 export default function SearchCandidatesPage() {
   return (
     <>
@@ -17,44 +19,8 @@ export default function SearchCandidatesPage() {
       </div>
 
       <div className="mx-auto max-w-2xl">
-        <div className="my-4 flex w-full items-center justify-between rounded border border-black p-4">
-          <button>Candidate Name</button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="my-4 flex w-full items-center justify-between rounded border border-black p-4">
-          <button>Candidate Name</button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-              />
-            </svg>
-          </button>
-        </div>
+        <Candidate />
+        <Candidate />
       </div>
     </>
   );
